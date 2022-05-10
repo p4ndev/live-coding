8d0428f77e04f867f58c5b21656b896170e934f2
DESKTOP-TSL8SPM\Developer <gustavo_hen@hotmail.com>
2022-05-10 17:59:19 -0300

    * Fixing files and folders

---
+++ FrequencyCounterInStringArray.cs
@@ -0,0 +1,20 @@
+﻿
+FrequencyCounterInStringArray(new[] { "a", "ab", "abc" }, new[] { "ab", "abc", "abcd" });
+
+static IDictionary<char, int> FrequencyCounterInStringArray(string[] array1, string[] array2) {
+
+    IDictionary<char, int> res = new Dictionary<char, int>();
+
+    array1.Concat(array2).ToList().ForEach(all => {
+        foreach(var letter in all.ToCharArray()){
+            if(res.ContainsKey(letter))   res[letter]++;
+            else                          res.Add(letter, 1);
+        }
+    });
+
+    foreach(var item in res)
+        Console.WriteLine("Item {0} count {1}", item.Key, item.Value);
+
+    return res;
+
+}
