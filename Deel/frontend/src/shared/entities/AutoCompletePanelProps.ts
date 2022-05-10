92669fc7a55cdbe28cbf1ce1b3127d7fef0f0dad
DESKTOP-TSL8SPM\Developer <gustavo_hen@hotmail.com>
2022-05-10 17:36:54 -0300

    + Only Cynet project

--- Deel/frontend/src/shared/entities/AutoCompletePanelProps.ts
+++
@@ -1,6 +0,0 @@
-import IContact from "./IContact";
-
-export type AutoCompletePanelProps = {
-    source          : IContact[] | undefined,
-    onItemClicked   : (id : number) => void
-}
--- Globant/GoDaddy/LastUniqueFromIntArray_v3.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Globant.GoDaddy.v3;
-public static class LastUniqueFromIntArray{
-    public static int Find(int[] input) =>
-        input.LastOrDefault(
-            x => (
-                input.Count(y => y == x) == 1
-            )
-        );
-}
