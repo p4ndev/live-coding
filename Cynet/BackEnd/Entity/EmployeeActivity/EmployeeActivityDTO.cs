1292c2fb0e40e083290223285c5a0e8f57fcb12b
DESKTOP-TSL8SPM\Developer <gustavo_hen@hotmail.com>
2022-05-10 17:46:33 -0300

    + Only HackerRank project

--- Cynet/BackEnd/Entity/EmployeeActivity/EmployeeActivityDTO.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace BackEnd.Entity;
-
-public class EmployeeActivityDTO : EmployeeDTO{
-    public DateTime      Period          { get; set; }
-    public string        Action          { get; set; }
-}
---
+++ HackerRank/README.md
@@ -0,0 +1,9 @@
+# HackerRank
+
+https://hackerrank.com/worldcellos
+
+**English:** Some problems solved.
+
+**Português:** Alguns problemas resolvidos.
+
+**Español:** Algunos problemas resueltos.
