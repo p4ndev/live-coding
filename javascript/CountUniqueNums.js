51db164dc78e56e9a303fbfe59a64de82f77f77d
DESKTOP-TSL8SPM\Developer <gustavo_hen@hotmail.com>
2022-06-16 11:19:42 -0300

    + Javascript examples for classes * Should do on my own in Csharp

---
+++ javascript/CountUniqueNums.js
@@ -0,0 +1,19 @@
+
+function countUniqueNums(arr){
+    
+    if(arr.length === 0) return 0;
+    
+    let i = 0;
+    
+    for(let j = 0; i < arr.lenght; j++){
+        if(arr[i] !== arr[j]){
+            i++;
+            arr[i] = arr[j];
+        }
+    }
+    
+    return i + 1;
+    
+}
+
+countUniqueNums([1,1,1,1,2,3,4,4,5]); // 5
