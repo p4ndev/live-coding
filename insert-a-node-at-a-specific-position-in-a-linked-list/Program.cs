7bcf42d162f2c2f9e761147acd6b4ffb96b6bb7e
DESKTOP-TSL8SPM\Developer <gustavo_hen@hotmail.com>
2022-06-23 15:34:24 -0300

    + Done

---
+++ insert-a-node-at-a-specific-position-in-a-linked-list/Program.cs
@@ -0,0 +1,25 @@
+﻿
+SinglyLinkedListNode llist_head;
+SinglyLinkedList llist = new SinglyLinkedList();
+List<int> randomNumbers = new(){ 13,7 };
+
+// =================================================================
+
+randomNumbers.ForEach(x => {
+    llist_head = Result.InsertAtTheTail(llist.head, x);
+    llist.head = llist_head;
+});
+
+// =================================================================
+
+llist_head = Result.InsertAtHead(llist.head, 16);
+llist.head = llist_head;
+
+// =================================================================
+
+llist_head = Result.InsertNodeAtPosition(llist.head, 1, 2);
+llist.head = llist_head;
+
+// =================================================================
+
+Console.WriteLine("Done");
---
+++ insert-a-node-at-a-specific-position-in-a-linked-list/Result.cs
@@ -0,0 +1,74 @@
+﻿using static System.Console;
+
+public class SinglyLinkedList{
+
+    public SinglyLinkedListNode head;
+    public SinglyLinkedList() => this.head = null;
+
+}
+
+public class SinglyLinkedListNode{
+
+    public int data;
+    public SinglyLinkedListNode next;
+
+    public SinglyLinkedListNode(int nodeData){
+        this.data = nodeData;
+        this.next = null;
+    }
+
+}
+
+public static class Result{
+
+    public static SinglyLinkedListNode InsertAtTheTail(SinglyLinkedListNode head, int data) {
+
+        if (head is null)
+            return new SinglyLinkedListNode(data);
+
+        SinglyLinkedListNode tmp = head;
+
+        while (tmp.next != null)
+            tmp = tmp.next;
+
+        tmp.next = new SinglyLinkedListNode(data);
+
+        return head;
+
+    }
+
+    public static SinglyLinkedListNode InsertAtHead(SinglyLinkedListNode llist, int data) {
+
+        if (llist is null)
+            return new SinglyLinkedListNode(data);
+
+        SinglyLinkedListNode tmp = new(data);
+        
+        tmp.next = llist;
+
+        return tmp;
+
+    }
+
+    public static SinglyLinkedListNode InsertNodeAtPosition(SinglyLinkedListNode llist, int data, int position) {
+
+        if (llist is null) return new SinglyLinkedListNode(data);
+
+        SinglyLinkedListNode current = llist;
+
+        for (int step = 1; current.next != null; step++) {
+            
+            if (step == position){
+                var nno      = new SinglyLinkedListNode(data);                
+                nno.next     = current.next;
+                current.next = nno;
+            }
+
+            current = current.next;
+        }
+
+        return llist;
+
+    }
+
+}
