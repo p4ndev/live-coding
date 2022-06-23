51a49929ca3f742d5ed5ea3bb6d3b8c1184019b7
DESKTOP-TSL8SPM\Developer <gustavo_hen@hotmail.com>
2022-06-23 16:18:58 -0300

    + Delete a node + Insert a node at + Prepend a node (head) + Append a node (tail)

---
+++ delete-a-node-from-a-linked-list/Program.cs
@@ -0,0 +1,30 @@
+﻿
+SinglyLinkedListNode llist_head;
+SinglyLinkedList llist = new SinglyLinkedList();
+List<int> randomNumbers = new(){ 6,2,19,7,15,9 };
+
+// =================================================================
+
+// place all numbers linked
+randomNumbers.ForEach(x => {
+    llist_head = Result.InsertAtTheTail(llist.head, x);
+    llist.head = llist_head;
+});
+
+// =================================================================
+
+// place 20 at the beginning
+llist_head = Result.InsertAtHead(llist.head, 20);
+llist.head = llist_head;
+
+// =================================================================
+
+// place 6 before 15
+llist_head = Result.InsertNodeAtPosition(llist.head, 6, 5);
+llist.head = llist_head;
+
+// =================================================================
+
+// remove number 2
+llist_head = Result.RemoveNodeAtPosition(llist.head, 3);
+llist.head = llist_head;
