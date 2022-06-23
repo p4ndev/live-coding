b20753b01d1a6a7695d3ce2e5dfe4659b71708b3
DESKTOP-TSL8SPM\Developer <gustavo_hen@hotmail.com>
2022-06-23 17:18:43 -0300

    + Print + Print reversed

---
+++ print-the-elements-of-a-linked-list/Program.cs
@@ -0,0 +1,36 @@
+﻿
+SinglyLinkedListNode llist_head;
+SinglyLinkedList llist = new SinglyLinkedList();
+List<int> randomNumbers = new(){ 6,2,19,7,15,9 };
+
+// ================== Place all numbers linked =======================
+
+randomNumbers.ForEach(x => {
+    llist_head = Result.InsertAtTheTail(llist.head, x);
+    llist.head = llist_head;
+});
+
+// ================ Place 20 at the beginning ========================
+
+llist_head = Result.InsertAtHead(llist.head, 20);
+llist.head = llist_head;
+
+// ====================== Place 6 before 15 ==========================
+
+llist_head = Result.InsertNodeAtPosition(llist.head, 6, 5);
+llist.head = llist_head;
+
+// ======================= Remove number 2 ============================
+
+llist_head = Result.RemoveNodeAtPosition(llist.head, 3);
+llist.head = llist_head;
+
+// ======================= Print items ============================
+
+Result.Print(llist.head);
+Console.WriteLine("\n------------------------------------------------\n");
+
+// ======================= Reversed print ============================
+
+Result.ReversePrint(llist.head);
+Console.WriteLine("\n------------------------------------------------\n");
---
+++ print-the-elements-of-a-linked-list/Result.cs
@@ -0,0 +1,137 @@
+﻿using static System.Console;
+
+public class SinglyLinkedList{
+
+    public SinglyLinkedListNode head;
+    public SinglyLinkedList() => this.head = null;
+
+}
+
+public class SinglyLinkedListNode{
+
+    public int data;
+    public SinglyLinkedListNode next;
+
+    public SinglyLinkedListNode(int nodeData){
+        this.data = nodeData;
+        this.next = null;
+    }
+
+}
+
+public static class Result{
+
+    public static SinglyLinkedListNode InsertAtTheTail(SinglyLinkedListNode head, int data) {
+
+        if (head is null)
+            return new SinglyLinkedListNode(data);
+
+        SinglyLinkedListNode tmp = head;
+
+        while (tmp.next != null)
+            tmp = tmp.next;
+
+        tmp.next = new SinglyLinkedListNode(data);
+
+        return head;
+
+    }
+
+    public static SinglyLinkedListNode InsertAtHead(SinglyLinkedListNode llist, int data) {
+
+        if (llist is null)
+            return new SinglyLinkedListNode(data);
+
+        SinglyLinkedListNode tmp = new(data);
+        
+        tmp.next = llist;
+
+        return tmp;
+
+    }
+
+    public static SinglyLinkedListNode InsertNodeAtPosition(SinglyLinkedListNode llist, int data, int position) {
+
+        if (llist is null) return new SinglyLinkedListNode(data);
+
+        SinglyLinkedListNode current = llist;
+
+        for (int step = 1; current.next != null; step++) {
+            
+            if (step == position){
+                var nno      = new SinglyLinkedListNode(data);                
+                nno.next     = current.next;
+                current.next = nno;
+                current = current.next;
+                break;
+            }
+
+            current = current.next;
+        }
+
+        return llist;
+
+    }
+
+    public static SinglyLinkedListNode RemoveNodeAtPosition(SinglyLinkedListNode llist, int position) {
+        
+        if (llist is null) return null;
+
+        SinglyLinkedListNode current = llist;
+
+        for (int step = 1; current.next != null; step++){
+            
+            if (step == (position - 1)){
+                var dismiss  = current.next;
+                current.next = dismiss.next;
+                current      = current.next;
+                break;
+            }
+
+            current = current.next;
+
+        }
+
+        return llist;
+
+    }
+
+    public static void Print(SinglyLinkedListNode llist) {
+        
+        if (llist is null) return;
+
+        Queue<int> que = new();
+        SinglyLinkedListNode current = llist.next;
+
+        que.Enqueue(llist.data);
+
+        while (current != null) {
+            que.Enqueue(current.data);
+            current = current.next;
+        }
+
+        for (int i = 0, j = que.Count; i < j; i++)
+            Write("{0} ", que.Dequeue());
+
+    }
+
+    public static void ReversePrint(SinglyLinkedListNode llist){
+
+        if (llist is null) return;
+
+        Stack<int> stk = new();
+        SinglyLinkedListNode current = llist.next;
+        
+        stk.Push(llist.data);
+
+        while (current != null) {
+            stk.Push(current.data);
+            current = current.next;
+        }
+
+        for (int i = 0, j = stk.Count; i < j; i++)
+            Write("{0} ", stk.Pop());
+
+    }
+
+}
