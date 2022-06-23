ab5224b02b9f2e580aaf4e0b312e8f3d28086e4a
DESKTOP-TSL8SPM\Developer <gustavo_hen@hotmail.com>
2022-06-23 12:45:41 -0300

    + Done, on debugging it's working perfectly * They should review the order not asked on statement

---
+++ insert-a-node-at-the-head-of-a-linked-list/Program.cs
@@ -0,0 +1,18 @@
+﻿
+SinglyLinkedListNode llist_head;
+SinglyLinkedList llist = new SinglyLinkedList();
+List<int> randomNumbers = new(){ 383, 484, 392, 975 };
+
+// =================================================================
+
+randomNumbers.ForEach(x => {
+    llist_head = Result.InsertAtTheTail(llist.head, x);
+    llist.head = llist_head;
+});
+
+// =================================================================
+
+llist_head = Result.InsertAtHead(llist.head, 321);
+llist.head = llist_head;
+
+Console.WriteLine("Done");
